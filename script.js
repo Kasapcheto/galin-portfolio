60c62188dfa2b16d50aaf6760b41091f8e576428
Kasapcheto <galingergana@gmail.com>
2026-07-02 18:17:21 +0300

    okay

---
+++ script.js
@@ -0,0 +1,15 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 80) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
